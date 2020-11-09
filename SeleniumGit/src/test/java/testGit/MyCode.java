8db6fe038ed46c12b79274bd70f79d411556b620
aishu <aishu@LAPTOP-U35H9PEV>
2020-11-09 17:09:13 +0530

    First Commit

---
+++ SeleniumGit/src/test/java/testGit/MyCode.java
@@ -0,0 +1,19 @@
+package testGit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyCode {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E://chromedriver.exe");
+		ChromeDriver d=new ChromeDriver();
+		d.get("https://opensource-demo.orangehrmlive.com/");
+		d.findElement(By.name("txtUsername")).sendKeys("Admin");
+		d.findElement(By.id("txtPassword")).sendKeys("admin123");
+		d.findElement(By.id("btnLogin")).click();
+		
+	}
+
+}
